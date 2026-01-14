a9b3d5e252350929d966adfc10aad0f92f273b90
Sai Krishna Vennamaneni <saivnm5@gmail.com>
2026-01-14 20:13:01 +0530

    Fixing header, turning green

--- code/src/app/components/core/header.tsx
+++ code/src/app/components/core/header.tsx
@@ -5,14 +5,19 @@ import Image from "next/image";
 export default function Header() {
 
     return (
-        <header>
-            <div>
-                <Image
-                    src="./logo-black.png"
-                    alt="Logo"
-                    width={50}
-                    height={0}
-                />
+        <header className="relative pt-2 pl-2">
+            <Image
+                src="./logo-green.png"
+                alt="Logo"
+                width={65}
+                height={0}    
+                className="absolute left-2 top-2"
+            />
+            <div className="flex items-start gap-4 pl-18">
+                <div className="flex flex-col justify-start text-left">
+                    <h1>#local</h1>
+                    <p className="text-sm text-gray-600 !mt-1">a location based community platform</p>
+                </div>
             </div>
         </header>
     );
