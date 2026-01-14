77e1647e34702ca14d6133ac3536ff2b4e6543de
Sai Krishna Vennamaneni <saivnm5@gmail.com>
2026-01-14 20:19:00 +0530

    Header border

--- code/src/app/components/core/header.tsx
+++ code/src/app/components/core/header.tsx
@@ -16,8 +16,12 @@ export default function Header() {
             <div className="flex items-start gap-4 pl-18">
                 <div className="flex flex-col justify-start text-left">
                     <h1>#local</h1>
-                    <p className="text-sm text-gray-600 !mt-1">a location based community platform</p>
+                    <p className="text-sm text-gray-600 !mt-1 !mb-1">a location based community platform</p>
                 </div>
+            </div>
+
+            <div className="absolute bottom-0 left-0 right-0 flex justify-center">
+                <div className="h-[1px] w-2/4 bg-gradient-to-r from-transparent via-green to-transparent" />
             </div>
         </header>
     );
