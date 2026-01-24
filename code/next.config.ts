053af6df5686020daa3f424db54ed4dfe0ffa72a
Sai Krishna Vennamaneni <saivnm5@gmail.com>
2026-01-24 11:52:18 +0530

    Adding dashboard

--- code/next.config.ts
+++ code/next.config.ts
@@ -1,7 +1,16 @@
 import type { NextConfig } from "next";
+
 const nextConfig: NextConfig = {
   output: "export",
   images: { unoptimized: true },
+  async rewrites() {
+    return [
+      {
+        source: "/api/issues",
+        destination: "https://staging.api.smalltech.in/local/api/v1/issues",
+      },
+    ];
+  },
 };
 
 export default nextConfig;
