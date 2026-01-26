32fd4ac6c2e6d633bae81f51ef612e52f9cdadef
Vennamaneni Sai Krishna <saivnm5@gmail.com>
2026-01-26 19:41:50 +0530

    Shifting layout (#25)
    * Shifting layout
    
    * text

--- code/src/app/components/core/footer.tsx
+++ code/src/app/components/core/footer.tsx
@@ -1,9 +1,15 @@
+import Link from "next/dist/client/link";
+
 export default function Footer() {
     const currentYear = new Date().getFullYear();
     return (
         <footer>
             <span>
-                Made with ❤️ © {currentYear} smallTech, India
+                Made with ❤️ © {currentYear}{" "}
+                <Link href="https://smalltech.in/?utm_source=local&utm_medium=footer&utm_campaign=launch" target="_blank" rel="noopener noreferrer">
+                    smallTech
+                </Link>
+                , India
             </span>
         </footer>
     );
