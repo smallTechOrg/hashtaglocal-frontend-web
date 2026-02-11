7d96abbc7e70e3abc57d966bf547d9e61ff9b4d8
Sai Krishna Vennamaneni <saivnm5@gmail.com>
2026-02-11 12:15:11 +0530

    Adding Mumbai and Ajmer to home page

--- code/src/app/components/dashboard/dashboard.tsx
+++ code/src/app/components/dashboard/dashboard.tsx
@@ -33,7 +33,9 @@ interface IssuesResponse {
 const CITY_OPTIONS = [
   { label: "#bengaluru", value: "%23bengaluru" },
   { label: "#nashik", value: "%23nashik" },
+  { label: "#mumbai", value: "%23mumbai" },
   { label: "#sikar", value: "%23sikar" },
+  { label: "#ajmer", value: "%23ajmer" },
 ];
 
 
